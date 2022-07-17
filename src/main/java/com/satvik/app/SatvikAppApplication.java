253ec236bf4010c45b67e9c023bd99d2d484a097
Birudeo Garande <birudeogarande@gmail.com>
2022-07-17 19:13:42 +0530

    first commit

---
+++ src/main/java/com/satvik/app/SatvikAppApplication.java
@@ -0,0 +1,13 @@
+package com.satvik.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatvikAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatvikAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satvik/app/SatvikAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satvik.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatvikAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
